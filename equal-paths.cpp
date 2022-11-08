d4c4b97dc68f24e3413490c8db8db58f2037825a
Leyland Yang <lbyang@usc.edu>
2022-11-08 01:14:31 +0000

    equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -3,11 +3,30 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int findLeaf(Node* root, int level) {
+    if (root==NULL) {
+        return true;
+    }
+    if (root->left==NULL && root->right==NULL) {
+        return level;
+    }
+    return findLeaf(root->left, level+1);
+    return findLeaf(root->right, level+1);
+}
+bool checkLeaves(Node* root, int level, int leaf) {
+    if (root==NULL) {
+        return true;
+    }
+    if (root->left==NULL && root->right==NULL) {
+        return level==leaf;
+    }
+    return checkLeaves(root->left, level+1, leaf) && checkLeaves(root->right, level+1, leaf);
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    int level=0;
+    int leaf= findLeaf(root, level);
+    return checkLeaves(root, level, leaf);
 }
-
